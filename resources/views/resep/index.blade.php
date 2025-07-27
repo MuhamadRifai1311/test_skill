d13a3018997c814e67a930654c905436ad856d5b
muhamadrifai0210 <mr209488@gmail.com>
2025-07-27 14:39:31 +0700

    Update index handling jika data tidak ada

--- resources/views/resep/index.blade.php
+++ resources/views/resep/index.blade.php
@@ -1,12 +1,13 @@
 <x-app-layout>
-
     <div class="mx-auto p-4">
         <h1 class="text-2xl mb-4 font-bold">Resep List</h1>
+
         @if (session('success'))
             <div class="alert alert-success">
                 {{ session('success') }}
             </div>
         @endif
+
         <div class="flex flex-row justify-end items-center gap-2 mb-6 mt-4">
             <div>
                 <p class="text-lg">tambah resep :</p>
@@ -18,12 +19,11 @@
                 <a href="{{ route('resep.racikan.create') }}">
                     <button class="btn btn-primary text-md">Racikan</button>
                 </a>
-
             </div>
         </div>
+
         <div class="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
             <table class="table">
-                <!-- head -->
                 <thead>
                     <tr>
                         <th>No</th>
@@ -33,19 +33,24 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <!-- row 1 -->
-                    @foreach ($reseps as $resep)
+                    @forelse ($reseps as $resep)
                         <tr>
                             <td>{{ $loop->iteration }}</td>
                             <td>{{ $resep->resep_tipe }}</td>
                             <td>{{ \Carbon\Carbon::parse($resep->resep_tanggal)->format('d-M-y') }}</td>
-                            <td><a href="{{ route('resep.show', $resep->id) }}">
-                                    <button class="btn btn-info">Detail</button></a></td>
+                            <td>
+                                <a href="{{ route('resep.show', $resep->id) }}">
+                                    <button class="btn btn-info">Detail</button>
+                                </a>
+                            </td>
                         </tr>
-                    @endforeach
+                    @empty
+                        <tr>
+                            <td colspan="4" class="text-center text-gray-500">Belum ada data resep.</td>
+                        </tr>
+                    @endforelse
                 </tbody>
             </table>
         </div>
     </div>
-
 </x-app-layout>
